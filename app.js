544eda9eaa0f66b5716c2eb0d4363c7e79517d7a
esilberberg <ericsil218@gmail.com>
2024-03-23 14:46:18 -0400

    n3w d3s16n

--- app.js
+++ app.js
@@ -17,34 +17,56 @@ const institutionId = 'i111455621';
 
 fetchData(institutionId)
   .then(data => {
-    data['results'].forEach(e => {
+      const dataSlice =  data['results'].slice(0, 24);
+      dataSlice.forEach((e, index) => {
       const title = e['title'];
-      const publication_year = e['publication_year'];
       const doi = e['doi'];
-      let topic = '';
-      if (e['primary_topic']?.['display_name']) {
-        topic = e['primary_topic']['display_name'];
-      }
-      let publication = '';
-      if (e['primary_location']?.['source']?.['display_name']) {
-        publication = e['primary_location']['source']['display_name'];
-      }
       
       const htmlOutput = `
         <div class="citation">
-          <div class="title"><a href="${doi}" target="_blank" rel="noopener noreferrer">${title}</a></div>
-          <div class="publication">${publication}</div>
-          <div class="authorship">
-            ${e['authorships'].map(a => `
-              <div class="author">
-                <span class="author-name">${a['raw_author_name']}</span>
-                <span class="author-affiliation">${a['institutions']?.[0]?.['display_name'] || ''}</span>
-              </div>
-            `).join('')}
-          </div>
+          <a href="${doi}" target="_blank" rel="noopener noreferrer">
+            <div class="title">${index + 1}. ${title}</div>
+          </a>
         </div>
       `;
 
       display.insertAdjacentHTML('beforeend', htmlOutput);
     });
   });
+
+
+
+// fetchData(institutionId)
+//   .then(data => {
+//     const firstTenResults = data['results'].slice(0, 10);
+//     firstTenResults.forEach(e => {
+//       const title = e['title'];
+//       const publication_year = e['publication_year'];
+//       const doi = e['doi'];
+//       let topic = '';
+//       if (e['primary_topic']?.['display_name']) {
+//         topic = e['primary_topic']['display_name'];
+//       }
+//       let publication = '';
+//       if (e['primary_location']?.['source']?.['display_name']) {
+//         publication = e['primary_location']['source']['display_name'];
+//       }
+      
+//       const htmlOutput = `
+//         <div class="citation"><a href="${doi}" target="_blank" rel="noopener noreferrer">
+//           <div class="title">${title}</div>
+//           <div class="publication">${publication}</div>
+//           <div class="authorship">
+//             ${e['authorships'].map(a => `
+//               <div class="author">
+//                 <span class="author-name">${a['raw_author_name']}</span>
+//                 <span class="author-affiliation">${a['institutions']?.[0]?.['display_name'] || ''}</span>
+//               </div>
+//             `).join('')}
+//           </div>
+//         </a></div>
+//       `;
+
+//       display.insertAdjacentHTML('beforeend', htmlOutput);
+//     });
+//   });
