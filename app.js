358cb10b9f6a7c8045ed025174a1085b5a5fe373
esilberberg <ericsil218@gmail.com>
2024-08-21 22:52:18 -0400

    v2

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const display = document.getElementById('display')
+const summary = document.getElementById('summary')
+const institutionId = 'i111455621'; // Queens College OpenAlex I
 
 async function fetchData(institutionId) {
     try {
@@ -12,61 +14,61 @@ async function fetchData(institutionId) {
     }
   }
 
-// Queens College OpenAlex ID
-const institutionId = 'i111455621'; 
-
 fetchData(institutionId)
   .then(data => {
-      const dataSlice =  data['results'].slice(0, 24);
-      dataSlice.forEach((e, index) => {
-      const title = e['title'];
-      const doi = e['doi'];
-      
-      const htmlOutput = `
-        <div class="citation">
-          <a href="${doi}" target="_blank" rel="noopener noreferrer">
-            <div class="title">${index + 1}. ${title}</div>
-          </a>
-        </div>
-      `;
+    let topicsSummary = '';
+    const allTopicsList = data.results.reduce((acc, item) => {
+      return acc.concat(item.topics.map(({ display_name }) => display_name));
+    }, []);
+    const randomTopics = allTopicsList.slice(0).sort(() => 0.5 - Math.random()).slice(0, 4);
+    
+    randomTopics.forEach((topic) => {
+      topicsSummary += `<li class="topic">${topic}.</li>`
+    })
 
-      display.insertAdjacentHTML('beforeend', htmlOutput);
+    const summaryOutput = `Queens College faculty actively research and publish in wide variety of disciplines. This week, some of their newly published work focuses on:
+    <ul>${topicsSummary}</ul>
+    Scroll down to read their work.
+    `
+    summary.innerHTML = summaryOutput
+
+    data.results.forEach((e, index) => {
+      const title = e.title || 'No title available';
+      const doi = e.doi || '#';
+      const topics = e.topics || [];
+      const location = (e.locations[0] && e.locations[0].source.display_name) || 'Location unknown';
+      const authors = e.authorships || [];
+
+      let topicsList = '';
+      topics.forEach((topic, index) => {
+        topicsList += `<span class="topic">${topic.display_name}</span>`;
+        if (index < topics.length - 1) {
+          topicsList += '; ';
+        };
+      });
+
+      let authorList = '';
+      authors.forEach(author => {
+        const authorName = author.raw_author_name || 'Unknown author';
+        const authorAffiliation = (author.institutions[0] && author.institutions[0].display_name) || 'Affiliation unknown';
+        authorList += `<p class="author"><span class="author-name">${authorName},</span><span class="author-affiliation">${authorAffiliation}</span></p>`;
+      });
+      
+      const citationOutput = `
+        <a class ="cite-link" href="${doi}" target="_blank" rel="noopener noreferrer">
+        <div class="citation">
+          <div class="title">${index + 1}. ${title}</div>
+          <div class="description">
+            <div class="author-list">${authorList}</div>
+            <div class="label">Publication:</div>
+            <div class="location">${location}</div>
+            <div class="label">Topics:</div>
+            <div class="topics-list">${topicsList}</div>
+           </div>
+        </div>
+        </a>
+      `;
+      
+      display.insertAdjacentHTML('beforeend', citationOutput);
     });
   });
-
-
-
-// fetchData(institutionId)
-//   .then(data => {
-//     const firstTenResults = data['results'].slice(0, 10);
-//     firstTenResults.forEach(e => {
-//       const title = e['title'];
-//       const publication_year = e['publication_year'];
-//       const doi = e['doi'];
-//       let topic = '';
-//       if (e['primary_topic']?.['display_name']) {
-//         topic = e['primary_topic']['display_name'];
-//       }
-//       let publication = '';
-//       if (e['primary_location']?.['source']?.['display_name']) {
-//         publication = e['primary_location']['source']['display_name'];
-//       }
-      
-//       const htmlOutput = `
-//         <div class="citation"><a href="${doi}" target="_blank" rel="noopener noreferrer">
-//           <div class="title">${title}</div>
-//           <div class="publication">${publication}</div>
-//           <div class="authorship">
-//             ${e['authorships'].map(a => `
-//               <div class="author">
-//                 <span class="author-name">${a['raw_author_name']}</span>
-//                 <span class="author-affiliation">${a['institutions']?.[0]?.['display_name'] || ''}</span>
-//               </div>
-//             `).join('')}
-//           </div>
-//         </a></div>
-//       `;
-
-//       display.insertAdjacentHTML('beforeend', htmlOutput);
-//     });
-//   });
